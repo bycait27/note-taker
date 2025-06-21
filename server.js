f3249c3dfccb6752d7eae872310746c2177a86e8
bycait27 <caitlinba27@gmail.com>
2025-06-21 14:29:16 -0500

    Cleaned up uuid.js and server.js

--- server.js
+++ server.js
@@ -1,114 +1,113 @@
 // import necessary modules
-const express = require('express');
-const path = require('path');
-const noteData = require('./db/db.json');
-const uuid = require('./helpers/uuid.js');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const noteData = require("./db/db.json");
+const uuid = require("./helpers/uuid.js");
+const fs = require("fs");
 
-// declare app variable
+// declare app and PORT variables
 const app = express();
-// declare PORT variable
 const PORT = process.env.PORT || 3001;
 
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 
 // invoke app.use() and serve static files from the public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // create a route that will serve the index.html file to user
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html')) // send file to browser
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html")); // send file to browser
 });
 
 // create a route that will serve the notes.html file to the user when clicking 'Get Started'
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // retrieve notes data
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
 
-        const parsedData = JSON.parse(data);
+    const parsedData = JSON.parse(data);
 
-        res.json(parsedData);
-    });
+    res.json(parsedData);
+  });
 });
 
 // post request to post a new note
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a new note`);
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuid(),
-    };
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a new note`);
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid(),
+  };
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
 
-        const parsedData = JSON.parse(data);
-        parsedData.push(newNote);
+    const parsedData = JSON.parse(data);
+    parsedData.push(newNote);
 
-        fs.writeFile('./db/db.json', JSON.stringify(parsedData), (writeErr) => {
-            if (writeErr) {
-                console.error(writeErr);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
+    fs.writeFile("./db/db.json", JSON.stringify(parsedData), (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
 
-            const response = {
-                status: 'Success!',
-                body: newNote,
-            };
+      const response = {
+        status: "Success!",
+        body: newNote,
+      };
 
-            console.log(response);
-            res.status(201).json(response);
-        });
+      console.log(response);
+      res.status(201).json(response);
     });
+  });
 });
 
 // delete request to delete a note
-app.delete('/api/notes/:id', (req, res) => {
-    console.info(`${req.method} request received to delete a note`);
+app.delete("/api/notes/:id", (req, res) => {
+  console.info(`${req.method} request received to delete a note`);
 
-    const noteId = req.params.id;
-    console.log('noteId:', noteId);
+  const noteId = req.params.id;
+  console.log("noteId:", noteId);
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
 
-        const parsedData = JSON.parse(data);
-        const newData = parsedData.filter((note) => note.id !== noteId);
+    const parsedData = JSON.parse(data);
+    const newData = parsedData.filter((note) => note.id !== noteId);
 
-        fs.writeFile('./db/db.json', JSON.stringify(newData), (writeErr) => {
-            if (writeErr) {
-                console.error(writeErr);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
+    fs.writeFile("./db/db.json", JSON.stringify(newData), (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
 
-            const response = {
-                status: 'Success!',
-                body: newData,
-            };
+      const response = {
+        status: "Success!",
+        body: newData,
+      };
 
-            console.log(response);
-            res.status(201).json(response);
-        });
+      console.log(response);
+      res.status(201).json(response);
     });
+  });
 });
 
 // console logs 'App is listening at http://localhost:3001'
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`);
 });
